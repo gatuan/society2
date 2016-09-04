637378ad8007352688281dc07d57ce3dec59be42
George Atuan <Iyados@Iyads-MBP.iptv.microsoft.com>
2016-09-03 22:05:01 -0300

    screen working

---
+++ Society2/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Society2
+//
+//  Created by George Atuan on 9/3/16.
+//  Copyright © 2016 George Atuan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 155.0 / 255.0
